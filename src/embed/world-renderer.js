815d6c143a81da9fc983adc46023a58dec871d23
Gabriele Fusi <gabfusi@gmail.com>
2017-07-04 15:11:30 +0200

    removed reticle renderer

--- src/embed/world-renderer.js
+++ src/embed/world-renderer.js
@@ -16,7 +16,7 @@ var AdaptivePlayer = require('./adaptive-player');
 var EventEmitter = require('eventemitter3');
 var Eyes = require('./eyes');
 var HotspotRenderer = require('./hotspot-renderer');
-var ReticleRenderer = require('./reticle-renderer');
+//var ReticleRenderer = require('./reticle-renderer');
 var SphereRenderer = require('./sphere-renderer');
 var EditorRenderer = require('./editor-renderer');
 var TWEEN = require('tween.js');
@@ -48,7 +48,7 @@ function WorldRenderer() {
     this.hotspotRenderer = new HotspotRenderer(this);
     this.hotspotRenderer.on('focus', this.onHotspotFocus_.bind(this));
     this.hotspotRenderer.on('blur', this.onHotspotBlur_.bind(this));
-    this.reticleRenderer = new ReticleRenderer(this.camera);
+    //this.reticleRenderer = new ReticleRenderer(this.camera);
     this.editorRenderer = new EditorRenderer(this);
     this.currentVideoTime = { currentTime: 0};
 
