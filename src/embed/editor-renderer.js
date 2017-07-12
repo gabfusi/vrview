de4d2031d6c83dbc736e93f67901792de990433a
Gabriele Fusi <gabfusi@gmail.com>
2017-07-12 15:36:20 +0200

    quaternion fix

--- src/embed/editor-renderer.js
+++ src/embed/editor-renderer.js
@@ -193,15 +193,16 @@ EditorRenderer.prototype.getShapeAnimationPercentage_ = function (shape_id, fram
       for (var j = 0, ll = shapeKeyframes[i].vertices.length; j < ll; j++) {
 
         // add rotation quaternion to keyframe if not exists
-        if (typeof shapeKeyframes[i].vertices[j].quaternion === 'undefined' && shapeKeyframes[i + 1]) {
+        if (typeof shapeKeyframes[i].vertices[j].quaternion === 'undefined') {
 
-            Q1 = shapeKeyframes[i].vertices[j];
-            Q2 = shapeKeyframes[i + 1].vertices[j];
+          Q1 = shapeKeyframes[i].vertices[j];
+          Q2 = shapeKeyframes[i + 1].vertices[j];
 
-            shapeKeyframes[i].vertices[j].quaternion = (new THREE.Quaternion()).setFromUnitVectors(Q1.normalize(), Q2.normalize());
+          shapeKeyframes[i].vertices[j].quaternion = (new THREE.Quaternion()).setFromUnitVectors(Q1.normalize(), Q2.normalize());
 
-            // console.debug('Created quaternion for shape ' + shape_id + ' from frame ' + shapeKeyframes[i].frame + ' and frame ' + shapeKeyframes[i + 1].frame)
-
+          console.debug('Created quaternion for shape ' + shape_id + ' from frame ' + shapeKeyframes[i].frame + ' and frame ' + shapeKeyframes[i + 1].frame)
+        } else {
+          console.debug('Quaternion exists for shape ' + shape_id + ' at frame ' + shapeKeyframes[i].frame)
         }
 
       }
